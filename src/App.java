2ec34879c771ad4dd30bada20d471cc2b60348f9
sathwi6 <sathwikachowdary.y@gmail.com>
2025-01-07 11:44:31 +0530

    second commit

---
+++ src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) {
+        int x=2,y=5;
+        if(x>y){
+            System.out.println("x is greater");
+        }
+        else{
+            System.err.println("y is greater");
+        }
+    }
+}
---
+++ src/Day_5_Loops.java
@@ -0,0 +1,20 @@
+
+public class Day_5_Loops {
+    public static void main(String[] args){
+        String c="sathwika";
+        System.out.println(c.length());
+        System.out.println(c.charAt(0));
+        System.out.println(c.substring(2,3));
+        System.out.println("hello".equals("Hello"));
+        System.out.println(c.substring(c.length()-1));
+        System.out.println(c.substring(c.length() - 3));
+        System.out.println("a"+"b");
+        String result=1+"b";
+        System.out.println("hello"+(2+2));
+        for(int count=1;count<=10;count++){
+        System.out.println("Sathwikaaaaaaaaaaa");
+            
+        }
+        
+    }
+}
