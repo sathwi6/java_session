c3a08e28cc1bfdf87825e22b66481ed9e9a6e242
sathwi6 <sathwikachowdary.y@gmail.com>
2025-01-10 14:32:32 +0530

    Add files via upload

---
+++ Day_7.java
@@ -0,0 +1,19 @@
+
+public class Day_7 {
+    String name;
+    int id;
+    String classNo;
+    String dept;
+    int phnNumber;
+
+    public void talking(){
+        System.out.println("Students are talking");
+    }
+    public void makingNoise(){
+        System.out.println("Students are making noise");
+    }
+    public void coding(){
+        System.out.println("Students are coding");
+    }
+    }    
+    
