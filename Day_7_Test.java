711af66ccaaeb88886349227f83a76f5198190ae
sathwi6 <sathwikachowdary.y@gmail.com>
2025-01-10 14:32:48 +0530

    Add files via upload

---
+++ Day_7_Test.java
@@ -0,0 +1,12 @@
+public class Day_7_Test {
+    public static void main(String[] args) {
+        int[] arr=new int[5];
+        Day_7 std1=new Day_7();
+        System.out.println(std1.name);
+        std1.name="Sathwika";
+        std1.id=90;
+        std1.classNo="G2";
+        std1.dept="cse";
+        System.out.println(std1);
+    }
+}
